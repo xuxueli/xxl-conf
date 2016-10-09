80ff328646e4ff848f1b7158aca5618b0b2c16db
xueli.xue <xueli.xue@dianping.com>
2016-10-09 14:38:38 +0800

    更新RM

--- xxl-conf-admin/src/main/webapp/static/js/conf.1.js
+++ xxl-conf-admin/src/main/webapp/static/js/conf.1.js
@@ -123,7 +123,7 @@ $(function(){
     // jquery.validate 自定义校验 “英文字母开头，只含有英文字母、数字和下划线”
     jQuery.validator.addMethod("myValid01", function(value, element) {
         var length = value.length;
-        var valid = /^[a-z][a-z0-9_]*$/;
+        var valid = /^[a-z][a-z0-9.]*$/;
         return this.optional(element) || valid.test(value);
     }, "KEY只能由小写字母、数字和.组成,须以小写字母开头");
 
