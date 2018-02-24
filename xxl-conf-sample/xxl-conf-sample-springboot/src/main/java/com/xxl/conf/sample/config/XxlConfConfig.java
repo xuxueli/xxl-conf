74238edcf08b050616415a39914bf7dfbd07a295
xuxueli <931591021@qq.com>
2018-02-24 20:48:40 +0800

    Sample项目目录结构规范

---
+++ xxl-conf-sample/xxl-conf-sample-springboot/src/main/java/com/xxl/conf/sample/config/XxlConfConfig.java
@@ -0,0 +1,25 @@
+package com.xxl.conf.sample.config;
+
+import com.xxl.conf.core.spring.XxlConfFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * xxl-conf config
+ *
+ * @author xuxueli 2017-04-28
+ */
+@Configuration
+public class XxlConfConfig {
+    private Logger logger = LoggerFactory.getLogger(XxlConfConfig.class);
+
+    @Bean(initMethod = "init", destroyMethod = "destroy")
+    public XxlConfFactory xxlConfFactory() {
+        XxlConfFactory xxlConf = new XxlConfFactory();
+        logger.info(">>>>>>>>>>> xxl-conf config init.");
+        return xxlConf;
+    }
+
+}
