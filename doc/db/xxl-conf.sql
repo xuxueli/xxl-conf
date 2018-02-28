87569e048486f9f6402b276bb6281f54e7926f85
xuxueli <931591021@qq.com>
2018-02-28 21:56:24 +0800

    更新RM

--- doc/db/xxl-conf.sql
+++ doc/db/xxl-conf.sql
@@ -9,7 +9,7 @@ CREATE TABLE `xxl_conf_project` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `xxl_conf_node` (
-  `key` varchar(100) NOT NULL COMMENT '配置Key',
+  `key` varchar(200) NOT NULL COMMENT '配置Key',
   `appname` varchar(100) NOT NULL COMMENT '所属项目AppName',
   `title` varchar(100) NOT NULL COMMENT '配置描述',
   `value` varchar(2000) DEFAULT NULL COMMENT '配置Value',
@@ -18,7 +18,7 @@ CREATE TABLE `xxl_conf_node` (
 
 
 INSERT INTO `xxl_conf_project` VALUES ('default', '示例项目');
-INSERT INTO `xxl_conf_node` VALUES ('default.key01', 'default', '测试配置01', '一111'), ('default.key02', 'default', '测试配置02', '二222'), ('default.key03', 'default', '测试配置03', '三333');
+INSERT INTO `xxl_conf_node` VALUES ('default.key01', 'default', '测试配置01', '一1111'), ('default.key02', 'default', '测试配置02', '二2222'), ('default.key03', 'default', '测试配置03', '三3333');
 
 
 COMMIT;
