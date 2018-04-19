84712bfceb940a91788098bd110b39faf7bed2be
许雪里 <931591021@qq.com>
2018-04-19 21:14:32 +0800

    Update ConfigConstant.java

--- xxl-conf-example/src/main/java/com/xxl/conf/example/core/constant/ConfigConstant.java
+++ xxl-conf-example/src/main/java/com/xxl/conf/example/core/constant/ConfigConstant.java
@@ -1,16 +1,1 @@
-package com.xxl.conf.example.core.constant;
 
-import com.xxl.conf.core.annotation.XxlValue;
-
-/**
- * 测试使用，可删除
- * User: chenchen_839@126.com
- */
-public class ConfigConstant {
-    @XxlValue(key = "default.appname")
-    private String value;
-
-    public String getValue() {
-        return value;
-    }
-}
