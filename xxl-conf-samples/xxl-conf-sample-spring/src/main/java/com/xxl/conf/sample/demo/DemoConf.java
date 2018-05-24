2359cc09109b0c214fe3be0ce2405b2628f7cc19
xuxueli <931591021@qq.com>
2018-05-24 16:24:43 +0800

    更新RM

--- xxl-conf-samples/xxl-conf-sample-spring/src/main/java/com/xxl/conf/sample/demo/DemoConf.java
+++ xxl-conf-samples/xxl-conf-sample-spring/src/main/java/com/xxl/conf/sample/demo/DemoConf.java
@@ -1,19 +1,13 @@
 package com.xxl.conf.sample.demo;
 
 import com.xxl.conf.core.annotation.XxlConf;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 
 /**
  *  测试示例（可删除）
  *
  *  @author xuxueli
  */
-@Component
 public class DemoConf {
-
-
-	// ------------- XXL-CONF，配置数据从配置中心获取 -------------
 
 	/**
 	 * XXL-CONF：@XxlConf 注解方式
@@ -29,25 +23,6 @@ public class DemoConf {
 
 	public void setParamByXml(String paramByXml) {
 		this.paramByXml = paramByXml;
-	}
-
-
-	// ------------- 原生方式，配置数据从本地配置文件获取 -------------
-
-	/**
-	 * 原生方式：@Value 注解方式
-	 */
-	@Value("${local.anno}")
-	public String localByAnno;
-
-
-	/**
-	 * 原生方式：XML占位符方式
-	 */
-	public String localByXml;
-
-	public void setLocalByXml(String localByXml) {
-		this.localByXml = localByXml;
 	}
 
 }
