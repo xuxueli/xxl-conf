dd84ba30cb491ea6ae96fb59419584b299272f2e
xuxueli <931591021@qq.com>
2018-10-25 08:44:27 +0800

    更新RM

--- xxl-conf-admin/src/main/java/com/xxl/conf/admin/XxlConfApplication.java
+++ xxl-conf-admin/src/main/java/com/xxl/conf/admin/XxlConfApplication.java
@@ -1,13 +1,11 @@
 package com.xxl.conf.admin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author xuxueli 2018-06-08 20:55:06
  */
-@EnableAutoConfiguration
 @SpringBootApplication
 public class XxlConfApplication {
 
